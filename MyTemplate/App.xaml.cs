7114e2d15a8125df461e29c97057ba49d76ecce1
arblic <arblic000110110@gmail.com>
2021-09-05 12:31:45 +0900

    ソリューション追加

---
+++ MyTemplate/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Reflection;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyTemplate {
+
+	/// <summary>
+	/// App.xaml の相互作用ロジック
+	/// </summary>
+	public partial class App : Application {
+
+		/// <summary>
+		/// 開始オーバーライド
+		/// </summary>
+		protected override void OnStartup( StartupEventArgs e )
+		#region
+		{
+			/* 多重起動を禁止する場合 */
+			/*
+			var mutex = new Mutex( false, Assembly.GetExecutingAssembly().GetName().Name );
+			if( !mutex.WaitOne( 0, false ) ) {
+				MessageBox.Show( "このアプリケーションは多重起動出来ません。" );
+				Shutdown();
+			}
+			*/
+		}
+		#endregion
+	}
+}
---
+++ MyTemplate/MainWindow/MainWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Media;
+
+namespace MyTemplate.ViewModels {
+
+	public class MainWindowViewModel : INotifyPropertyChanged {
+
+		/// <summary>
+		/// ctor
+		/// </summary>
+		public MainWindowViewModel()
+		#region
+		{
+			var asm = Assembly.GetExecutingAssembly();
+			string titleName = "";
+
+			var versionInfo = FileVersionInfo.GetVersionInfo( asm.Location );
+
+			titleName += Path.GetFileNameWithoutExtension( asm.Location );
+			titleName += " ver.";
+			titleName += versionInfo.FileVersion;
+
+			TitleName = titleName;
+		}
+		#endregion
+
+
+		/// <summary>
+		/// タイトルバー名
+		/// </summary>
+		private string m_titleName = "";
+		public string TitleName
+		#region
+		{
+			get {
+				return m_titleName;
+			}
+			set {
+				if( m_titleName != value ) {
+					m_titleName = value;
+					RaisePropertyChanged( nameof( TitleName ) );
+				}
+			}
+		}
+		#endregion
+
+		/// <summary>
+		/// プロパティ変更通知
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void RaisePropertyChanged( string propertyname )
+		#region
+		{
+			PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( propertyname ) );
+		}
+		#endregion
+
+	}
+}
